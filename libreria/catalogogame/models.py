7479eceb5558d9b4aea003e8cda6815d417dc55c
Camilo Astroza Anabalon <el.camiloawsm@gmail.com>
2020-10-16 01:27:23 -0300

    Segundo commit

--- libreria/catalogogame/models.py
+++ libreria/catalogogame/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Game(models.Model):
+    titulo = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='ID Unico del VideoJuego')
+
+    descripcion = models.TextField(max_length=1000, help_text='Ingrese una breve descripcion del VideoJuego')
+    creador = models.CharField(max_length=200)
+    tipogame = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.titulo
